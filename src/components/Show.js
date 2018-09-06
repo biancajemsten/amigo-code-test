6c63208529fc6631eb088c7e709edf607a1fec56
Bianca Jemsten <biancajemsten@gmail.com>
2018-09-06 17:38:34 +0100

    enabled favoriting

--- src/components/Show.js
+++ src/components/Show.js
@@ -6,11 +6,14 @@ class NeoShow extends React.Component {
   constructor(){
     super();
     this.state = {
-      neo: []
+      neo: [],
+      favorite: false
     };
   }
 
   componentWillMount(){
+    // if(localStorage.includes(this.state.neo.id)) this.setState({favorite: true});
+
     axios({
       url: `https://api.nasa.gov/neo/rest/v1/neo/${this.props.match.params.id}?api_key=NXdg6uoUVxUVonkRFoatgFJBvv6AyMeCvQy332u4`,
       method: 'GET'
@@ -25,10 +28,32 @@ class NeoShow extends React.Component {
           diameterInM: (estimatedDiameter[1].estimated_diameter_min+estimatedDiameter[1].estimated_diameter_max)/2,
           diameterInFt: (estimatedDiameter[3].estimated_diameter_min+estimatedDiameter[3].estimated_diameter_max)/2,
           hazardous: res.data.is_potentially_hazardous_asteroid,
-          close_approaches: close_approach.length
+          close_approaches: close_approach.length,
+          id: res.data.neo_reference_id
         };
+        if (Object.keys(localStorage).indexOf(neo.name) > -1) {
+          this.setState({favorite: true});
+        }
         this.setState({neo});
       });
+  }
+
+  favoriteIt = () => {
+    this.favorite = !this.favorite;
+    if(this.favorite) localStorage.setItem(`${this.state.neo.name}`,`${this.state.neo.id}`);
+    if (Object.keys(localStorage).indexOf(this.state.neo.name) > -1) {
+      console.log('is favorited');
+      this.setState({favorite: true});
+    }
+  }
+
+  checkIfFavorited = () => {
+    console.log(Object.keys(this.state.neo));
+    if (Object.keys(localStorage).indexOf(this.state.neo.name) > -1) {
+      console.log('has test1');
+      this.setState({favorite: true});
+    }
+    console.log('from check',this.state.favorite);
   }
 
 
@@ -37,6 +62,10 @@ class NeoShow extends React.Component {
     return(
       <div>
         <h1 className="title is-1">Near Earth Object {this.state.neo.name}</h1>
+        <div onClick={this.favoriteIt} className="favoriteDiv">
+          {this.state.favorite ? <i className="fas fa-star"></i> : <i className="far fa-star"></i>}
+          <p>Favorite this Neo</p>
+        </div>
         <table className='table is-hoverable is-bordered'>
           <tbody>
             <tr>
