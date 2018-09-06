0d43bbfda6f753424ef7494d60bbd038fb6639c7
Bianca Jemsten <biancajemsten@gmail.com>
2018-09-06 15:34:36 +0100

    created tables for index page from NASA data

--- src/components/Index.js
+++ src/components/Index.js
@@ -1,11 +1,13 @@
 import React from 'react';
 import axios from 'axios';
+import { Link } from 'react-router-dom';
 
 class NEOIndex extends React.Component {
   constructor(){
     super();
     this.state = {
-      neos: []
+      neos: [],
+      dates: []
     };
   }
 
@@ -14,22 +16,43 @@ class NEOIndex extends React.Component {
       url: 'https://api.nasa.gov/neo/rest/v1/feed?start_date=2018-09-04&end_date=2018-09-06&api_key=NXdg6uoUVxUVonkRFoatgFJBvv6AyMeCvQy332u4',
       method: 'GET'
     })
-      .then(res => console.log(res.data));
+      .then(res => {
+        console.log(res.data);
+        const dates = Object.keys(res.data.near_earth_objects);
+        const neos = Object.values(res.data.near_earth_objects);
+        this.setState({dates, neos});
+      });
   }
 
   render(){
+    console.log(this.state.neos);
+    if(!this.state.neos) return <h1 className="title is-1">Loading...</h1>;
     return(
       <div>
         <h1>NEO List</h1>
-        <table className="table">
-          <thead>
-            <tr>
-            </tr>
-          </thead>
-          <tbody>
-
-          </tbody>
-        </table>
+        {this.state.dates.map((date, index) =>
+          <div key={index}>
+            <h2 className="title is-2">{date}</h2>
+            <table className="table is-hoverable is-bordered">
+              <thead>
+                <tr>
+                  <th>Name</th>
+                  <th>Id</th>
+                  <th>More Info</th>
+                </tr>
+              </thead>
+              <tbody>
+                {this.state.neos[index].map((neo, i) =>
+                  <tr key={i}>
+                    <td>{neo.name}</td>
+                    <td>{neo.neo_reference_id}</td>
+                    <td><Link to={`/${neo.neo_reference_id}`}>Click</Link></td>
+                  </tr>
+                )}
+              </tbody>
+            </table>
+          </div>
+        )}
       </div>
 
     );
